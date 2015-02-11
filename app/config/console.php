ff57f97a2267d573f8f1af7d082e9a85e7fd60a1
noumo <noumohope@gmail.com>
2015-02-11 10:54:26 +0300

    + console commands

---
+++ app/config/console.php
@@ -0,0 +1,28 @@
+<?php
+$params = require(__DIR__ . '/params.php');
+$db = require(__DIR__ . '/db.php');
+
+return [
+    'id' => 'app-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log', 'gii'],
+    'controllerNamespace' => 'app\commands',
+    'modules' => [
+        'gii' => 'yii\gii\Module',
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+];
