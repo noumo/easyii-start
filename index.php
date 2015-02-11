56ce541c8131e337948cc4a331f8f7bd875ed867
noumo <noumohope@gmail.com>
2015-02-11 10:54:49 +0300

    set default config as web

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/vendor/autoload.php');
 require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/app/config/main.php');
+$config = require(__DIR__ . '/app/config/web.php');
 
 (new yii\web\Application($config))->run();
