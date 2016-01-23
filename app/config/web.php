fa74fac268698beca78a8515b9037011f94c97bc
boehsermoe <boehsermoe@me.com>
2016-01-23 09:30:56 +0100

    Config and Composer fixes

--- app/config/web.php
+++ app/config/web.php
@@ -16,7 +16,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'U76IjGsUsTqA8wHund3jdZ5DTCt3W3wn',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
